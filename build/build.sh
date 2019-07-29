eb3ac481cac95c778797a8a0088c157643a566c0
Ryan Jarrett <ryan@sparkdevelopment.co.uk>
2019-07-29 10:17:26 +0100

    Update composer (by script)

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "$0" )/.." && pwd )"
 echo $DIR
-composer update
+composer update --ignore-platform-reqs
 git add .
 git commit -m "Update composer (by script)"
 git push heroku master
